6d58516a9a9ef0f0a930bc452874cd0172e6f6ae
CmdrNorthpaw <thetuxkid@gmail.com>
2021-06-14 19:52:35 +0100

    Make empty resource locations possible

--- src/main/kotlin/uk/cmdrnorthpaw/needle/resources/ResourceLocation.kt
+++ src/main/kotlin/uk/cmdrnorthpaw/needle/resources/ResourceLocation.kt
@@ -10,20 +10,31 @@ import kotlinx.serialization.encoding.Decoder
 import kotlinx.serialization.encoding.Encoder
 
 @Serializable(with = ResourceLocation.Serializer::class)
-data class ResourceLocation(val namespace: String, val path: String) {
+data class ResourceLocation(val namespace: String, val path: String, private val isEmpty: Boolean = true) {
 
     object Serializer : KSerializer<ResourceLocation> {
         override val descriptor = PrimitiveSerialDescriptor("resource", PrimitiveKind.STRING)
 
         override fun serialize(encoder: Encoder, value: ResourceLocation) {
-            encoder.encodeString("${value.namespace}:${value.path}")
+            encoder.encodeString(
+                if (!value.isEmpty) "${value.namespace}:${value.path}" else ""
+            )
         }
 
         override fun deserialize(decoder: Decoder): ResourceLocation {
             val decoded = decoder.decodeString()
-            val (namespace, path) = decoded.split(":")
-            return ResourceLocation(namespace, path)
+            return if (decoded == "") EMPTY
+            else {
+                val (namespace, path) = decoded.split(":")
+                ResourceLocation(namespace, path)
+            }
+
         }
+    }
+
+    companion object {
+        @JvmStatic
+        val EMPTY = ResourceLocation("", "", true)
     }
 
 }
