65a1f9f38aeaaabc4a24d93e148acee78e595400
CmdrNorthpaw <samlindop@outlook.com>
2021-10-27 10:47:04 +0100

    Use Gson serializer in Colour

--- src/main/kotlin/uk/cmdrnorthpaw/needle/resources/Colour.kt
+++ src/main/kotlin/uk/cmdrnorthpaw/needle/resources/Colour.kt
@@ -1,25 +1,21 @@
 package uk.cmdrnorthpaw.needle.resources
 
-import kotlinx.serialization.KSerializer
-import kotlinx.serialization.Serializable
-import kotlinx.serialization.descriptors.PrimitiveKind
-import kotlinx.serialization.descriptors.PrimitiveSerialDescriptor
-import kotlinx.serialization.descriptors.SerialDescriptor
-import kotlinx.serialization.encoding.Decoder
-import kotlinx.serialization.encoding.Encoder
+import com.google.gson.JsonDeserializationContext
+import com.google.gson.JsonElement
+import com.google.gson.JsonPrimitive
+import com.google.gson.JsonSerializationContext
+import java.lang.reflect.Type
 
 @JvmInline
-@Serializable(with = Colour.Serializer::class)
 value class Colour(val hex: Int) {
-    object Serializer: KSerializer<Colour> {
-        override val descriptor = PrimitiveSerialDescriptor("colour", PrimitiveKind.STRING)
-
-        override fun serialize(encoder: Encoder, value: Colour) {
-            encoder.encodeString(value.hex.toString().removePrefix("0x"))
+    object Serializer: uk.cmdrnorthpaw.needle.model.Serializer<Colour> {
+        override fun deserialize(json: JsonElement?, typeOfT: Type?, context: JsonDeserializationContext?): Colour {
+            json!!
+            return Colour(json.asString.toInt(radix = 16))
         }
 
-        override fun deserialize(decoder: Decoder): Colour {
-            return Colour(decoder.decodeString().toInt())
+        override fun serialize(src: Colour?, typeOfSrc: Type?, context: JsonSerializationContext?): JsonElement {
+            return JsonPrimitive(src!!.hex.toString(16))
         }
     }
 }
